72ee93a98ee7c83298a1ff94f43f0d0b7ac81c64
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-03-24 10:43:31 -0400

    green check?

---
+++ public/my-module.js
@@ -0,0 +1,8 @@
+// another-module.js should export theShire, buckland, and HobbitSettlement.
+// my-module.js should import theShire and buckland; it should export theShire and statement.
+
+import { theShire, buckland } from './another-module.js';
+
+const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+
+export { theShire, statement };
